0772b082b5012bfa8cdd4cbed666cda725364e6e
GoldenShulkerStudios <goldenshulkerstudios@gmail.com>
2024-03-11 16:20:07 -0500

    Fallo de totems agregados

--- src/main/java/me/ewahv1/plugin/Listeners/DifficultityOnePlayers.java
+++ src/main/java/me/ewahv1/plugin/Listeners/DifficultityOnePlayers.java
@@ -2,27 +2,49 @@ package me.ewahv1.plugin.Listeners;
 
 import java.util.Random;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
 
 public class DifficultityOnePlayers implements Listener {
+    private Random random = new Random();
+    private int failProbability = 90; // Puedes cambiar este valor para ajustar la probabilidad de falla
 
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        Player player = event.getEntity();
-        if (player.getInventory().getItemInMainHand().getType() == Material.TOTEM_OF_UNDYING || player.getInventory().getItemInOffHand().getType() == Material.TOTEM_OF_UNDYING) {
-            if (new Random().nextInt(10) < 9) { // 90% de probabilidad
-                player.getInventory().removeItem(new ItemStack(Material.TOTEM_OF_UNDYING));
-                player.damage(10);
-                player.sendMessage(ChatColor.RED + "¡Tu tótem de inmortalidad falló en salvarte!");
+    public void onPlayerDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
+
+        Player player = (Player) event.getEntity();
+
+        if (player.getHealth() - event.getFinalDamage() > 0) {
+            return;
+        }
+
+        if (player.getInventory().contains(Material.TOTEM_OF_UNDYING)) {
+            Bukkit.broadcastMessage("El jugador " + player.getName() + " ha activado un totem");
+
+            if (random.nextInt(100) < failProbability) {
+                // Mata al jugador directamente
+                player.setHealth(0);
+                
+                // Cambia el modo de juego del jugador a espectador
+                Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                    public void run() {
+                        player.setGameMode(GameMode.SPECTATOR);
+                    }
+                }, 20);
+                
+                Bukkit.broadcastMessage("El totem de " + player.getName() + " ha fallado con una probabilidad del " + failProbability + "% de fallar");
             }
             else{
-                player.sendMessage(ChatColor.RED + "¡Tu tótem de inmortalidad funcionó en salvarte!");
+                Bukkit.broadcastMessage("El totem de " + player.getName() + " se ha activado con una probabilidad del " + failProbability + "% de fallar");
             }
         }
     }
